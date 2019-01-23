6a941aac99b1215ee454be52c4577007680f1971
Tom Miller <thomas.j.c.miller@outlook.com>
2019-01-23 17:51:38 +0000

    set state of visitorName

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import './App.css';
 
 class App extends Component {
   render() {
+    state = {
+      visitorName: ""
+    };
+
+    updateVisitorName = (name) => {
+      this.setState({ visitorName: name})
+    };
+
+    clearText = ( => {
+      this.setState({visitorName: ""})
+    });
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
     );
   }
 }
