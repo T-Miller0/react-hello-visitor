4f87757c4772aa3f6c095d161bd05de68bc72c26
Tom Miller <thomas.j.c.miller@outlook.com>
2019-01-24 15:39:28 +0000

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-    state = {
-      visitorName: ""
-    };
+class VisitorName extends React.Component {
+  handleChange = (e) => {
+    this.props.updateVisitorName(e.target.value);
+  };
 
-    updateVisitorName = (name) => {
-      this.setState({ visitorName: name})
-    };
-
-    clearText = () => {
-      this.setState({visitorName: ""})
-    };
-
-    render() {
+  render() {
     return (
-      <div>
-      <VisitorName visitorName={this.state.visitorName}
-        updateVisitorName={this.updateVisitorName} />
-      <button type='button' onClick={this.clearText}>Clear</button>
-      <br/>
-      <p>Hello, {this.state.visitorName || 'visitor'}</p>
-      </div>
+      <input type='text' placeholder='Enter your name'
+             value={this.props.visitorName}
+             onChange={this.handleChange} />
     );
   }
 }
 
-// ReactDOM.render(<App/>.document.getElementById('app'));
+class App extends React.Component {
+  state = {
+    visitorName: ''
+  };
+  updateVisitorName = (name) => {
+    this.setState({ visitorName: name });
+  };
+  clearText = () => {
+    this.setState({ visitorName: '' })
+  };
+  render() {
+    return (
+      <div>
+        <VisitorName visitorName={this.state.visitorName}
+                     updateVisitorName={this.updateVisitorName} />
+        <button type='button' onClick={this.clearText}>Clear</button>
+        <br />
+        <p>Hello, {this.state.visitorName || 'visitor'}</p>
+      </div>
+    );
+  }
+}
 export default App;
